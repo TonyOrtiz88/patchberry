05581c19b0a752435159895786b099caa0cc0557
Anthony Ortiz-Morris <anthony.ortiz.morris88@gmail.com>
2019-07-19 10:38:36 -0400

    schoolFinal

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -5,7 +5,7 @@ if (process.env.MLAB_USERNAME_WEBDEV) {
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = "mongodb://" + username + ":" + password;
-  connectionString += "@ds019268.mlab.com:19268/heroku_23wqkv57"; // use yours
+  connectionString += "@ds353007.mlab.com:53007/heroku_hjsg0lgj"; // use yours
 }
 
 var mongoose = require("mongoose");
